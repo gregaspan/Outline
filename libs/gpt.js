b3503f5b626271589ffe33c47c7bd166d4df9ee2
gregaspan <gregor.span@student.um.si>
2025-05-28 23:40:37 +0200

    feat: google genai

--- libs/gpt.js
+++ libs/gpt.js
@@ -1,50 +1,34 @@
-import axios from "axios";
+import { GoogleGenAI } from '@google/genai';
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 
-// Use this if you want to make a call to OpenAI GPT-4 for instance. userId is used to identify the user on openAI side.
-export const sendOpenAi = async (messages, userId, max = 100, temp = 1) => {
-  const url = "https://api.openai.com/v1/chat/completions";
+const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
 
-  console.log("Ask GPT >>>");
-  messages.map((m) =>
-    console.log(" - " + m.role.toUpperCase() + ": " + m.content)
-  );
-
-  const body = JSON.stringify({
-    model: "gpt-4",
-    messages,
-    max_tokens: max,
-    temperature: temp,
-    user: userId,
-  });
-
-  const options = {
-    headers: {
-      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-      "Content-Type": "application/json",
-    },
-  };
-
+export const sendGemini = async (messages, userId, max = 100, temp = 1) => {
   try {
-    const res = await axios.post(url, body, options);
-
-    const answer = res.data.choices[0].message.content;
-    const usage = res?.data?.usage;
-
-    console.log(">>> " + answer);
-    console.log(
-      "TOKENS USED: " +
-        usage?.total_tokens +
-        " (prompt: " +
-        usage?.prompt_tokens +
-        " / response: " +
-        usage?.completion_tokens +
-        ")"
+    console.log('Ask Gemini >>>');
+    messages.forEach((m) =>
+      console.log(` - ${m.role.toUpperCase()}: ${m.content}`)
     );
-    console.log("\n");
 
+    // Usually: { author: "user" | "system" | "assistant", content: string }
+    const contents = messages.map((m) => ({
+      author: m.role,
+      content: m.content,
+    }));
+
+    const response = await ai.models.generateMessage({
+      model: 'gemini-2.0-flash-001',
+      messages: contents,
+      temperature: temp,
+      maxOutputTokens: max,
+    });
+
+    const answer = response.candidates?.[0]?.message?.content || '';
+    console.log('>>> ' + answer);
+    console.log('\n');
     return answer;
   } catch (e) {
-    console.error("GPT Error: " + e?.response?.status, e?.response?.data);
+    console.error('Gemini Error:', e);
     return null;
   }
 };
