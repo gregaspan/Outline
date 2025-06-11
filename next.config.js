42b90112c9fee2fd96d51c7b0e917cc7c5e4bdf0
gregaspan <gregor.span@student.um.si>
2025-06-11 20:34:28 +0200

    Revert "pwa"
    This reverts commit e803380768dabbc0101ba59beb6910bfd02c1751.

--- next.config.js
+++ next.config.js
@@ -1,20 +1,14 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-});
-
-const nextConfig = withPWA({
+const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: [
+      // NextJS <Image> component needs to whitelist domains for src={}
       "lh3.googleusercontent.com",
       "pbs.twimg.com",
       "images.unsplash.com",
       "logos-world.net",
     ],
   },
-});
+};
 
 module.exports = nextConfig;
